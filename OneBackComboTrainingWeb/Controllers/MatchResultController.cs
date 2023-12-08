1605a6ec581b19f0d745c5419af33bf565dd2b4b
Chris_Ho <chris.ho@onelab.tw>
2023-12-08 09:42:35 +0800

    match result practis

---
+++ OneBackComboTrainingWeb/Controllers/MatchResultController.cs
@@ -0,0 +1,40 @@
+﻿namespace OneBackComboTrainingWeb.Controllers;
+
+public class MatchResultController
+{
+    private string _matchResult = "0:0(First Half)";
+    private string _matchResultRepo;
+    private string _previousMatchResult = "";
+    private int _homeScore;
+    private int _awayScore;
+    private string _period = "First Half";
+    private Dictionary<string, string> _matchResultLookup = new Dictionary<string, string>
+    {
+        {"homeGoal","H"},
+        {"awayGoal","A"},
+        {"periodChange",";"},
+    };
+
+    public string GetMatchResult()
+    {
+        return _matchResult;
+    }
+
+    public void ReceivedEvent(string soccerEvent)
+    {
+        _matchResultRepo = $"{_previousMatchResult}{_matchResultLookup[soccerEvent]}";
+
+        List<char> lsMatchResultChars = _matchResultRepo.ToCharArray().ToList();
+        _period = _matchResultRepo.Contains(";") ? "Second Half" : "First Half";
+        _homeScore = lsMatchResultChars.Count(x => x == 'H');
+        _awayScore = lsMatchResultChars.Count(x => x == 'A');
+
+        _matchResult = $"{_homeScore}:{_awayScore}({_period})";
+    }
+
+    public void GetMatchResultRepo(string matchResultRepo)
+    {
+        _previousMatchResult = matchResultRepo;
+
+    }
+}
---
+++ OneBackTests/MatchResultTests.cs
@@ -0,0 +1,92 @@
+﻿using NSubstitute.Routing.Handlers;
+using OneBackComboTrainingWeb.Controllers;
+
+namespace OneBackTests;
+
+[TestFixture]
+public class MatchResultTests
+{
+    private MatchResultController _matchResult;
+
+    [SetUp]
+    public void SetUp()
+    {
+        _matchResult = new MatchResultController();
+    }
+    [Test]
+    public void initial_match_result()
+    {
+        ScoreShouldBe("0:0(First Half)");
+    }
+    [Test]
+    public void home_goal_when_0_0_first_half()
+    {
+        GivenPreviousMatchResult("");
+        WhenReceivedEvent("homeGoal");
+        ScoreShouldBe("1:0(First Half)");
+    }
+    [Test]
+    public void home_goal_when_1_0_first_half()
+    {
+        GivenPreviousMatchResult("H");
+        WhenReceivedEvent("homeGoal");
+        ScoreShouldBe("2:0(First Half)");
+    }
+    [Test]
+    public void away_goal_when_1_0_first_half()
+    {
+        GivenPreviousMatchResult("H");
+        WhenReceivedEvent("awayGoal");
+        ScoreShouldBe("1:1(First Half)");
+    }
+    [Test]
+    public void period_change_when_1_0_first_half()
+    {
+        GivenPreviousMatchResult("H");
+        WhenReceivedEvent("periodChange");
+        ScoreShouldBe("1:0(Second Half)");
+    }
+    [Test]
+    public void period_change_when_0_0_first_half()
+    {
+        GivenPreviousMatchResult("");
+        WhenReceivedEvent("periodChange");
+        ScoreShouldBe("0:0(Second Half)");
+    }
+    [Test]
+    public void home_goal_when_0_0_second_half()
+    {
+        GivenPreviousMatchResult(";");
+        WhenReceivedEvent("homeGoal");
+        ScoreShouldBe("1:0(Second Half)");
+    }
+    [Test]
+    public void away_goal_when_frist_half_0_0_second_half_1_0()
+    {
+        GivenPreviousMatchResult(";H");
+        WhenReceivedEvent("awayGoal");
+        ScoreShouldBe("1:1(Second Half)");
+    }
+    [Test]
+    public void away_goal_when_frist_half_1_0_second_half_1_0()
+    {
+        GivenPreviousMatchResult("H;");
+        WhenReceivedEvent("awayGoal");
+        ScoreShouldBe("1:1(Second Half)");
+    }
+
+    private void GivenPreviousMatchResult(string matchResultRepo)
+    {
+        _matchResult.GetMatchResultRepo(matchResultRepo);
+    }
+
+    private void WhenReceivedEvent(string soccerEvent)
+    {
+        _matchResult.ReceivedEvent(soccerEvent);
+    }
+
+    private void ScoreShouldBe(string expected)
+    {
+        Assert.That(_matchResult.GetMatchResult(),Is.EqualTo(expected));
+    }
+}
